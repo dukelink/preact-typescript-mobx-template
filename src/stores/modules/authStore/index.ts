bd69f64ee3868684c1e996ee40f58ba331e20812
Ryan Christian <ryanchristian4427@gmail.com>
2019-10-31 01:30:34 +0000

    feat: Rolled services into a separate package

--- src/stores/modules/authStore/index.ts
+++ src/stores/modules/authStore/index.ts
@@ -1,6 +1,5 @@
 import { observable, action } from 'mobx';
-import ApiService from '../../../services/apiService';
-import {saveToken} from "../../../services/jwtService";
+import {ApiService, JwtService} from "ts-axios-jwt-toolkit2";
 import {AuthSuccessResponse, LoginUser} from "./types";
 import User from "../../../models/User";
 
@@ -26,9 +25,8 @@ export class AuthStore {
     @action login(credentials: LoginUser) {
         this.inProgress = true;
         this.errors = "";
-        console.log(credentials);
         return ApiService.post("users/login", credentials)
-            .then((data : any) => this.setLoginState(data.user))
+            .then(({data} : any) => this.setLoginState(data.user))
             .catch(action((err: { response: { body: { errors: any; }; }; }) => {
                 this.errors = err.response && err.response.body && err.response.body.errors;
                 throw err;
@@ -38,7 +36,7 @@ export class AuthStore {
 
     setLoginState(user: AuthSuccessResponse) {
         this.currentUser = new User(user.first_name + " " + user.last_name, user.email, user.token);
-        saveToken(user.token);
+        JwtService.saveToken(user.token);
     }
 }
 
